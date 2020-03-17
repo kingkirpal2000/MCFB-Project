b76e5740eda738827b8eccf185cae7c53c9d86d8
Sabir Kirpal <skirpal2000@gmail.com>
2020-03-16 19:44:07 -0700

    Server is now connected to retreive index.html on load up

--- serverInit.js
+++ serverInit.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+
+
+//Tells us what port this server is on
 app.listen(3000, () => console.log("Listening at port 3000"));
+
+//Lets get this server to grab index.html
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
