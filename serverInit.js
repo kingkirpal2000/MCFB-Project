b0dc75f605a63412c8ca468c06f54ed49f9db21a
Sabir Kirpal <skirpal2000@gmail.com>
2020-03-25 20:17:11 -0700

    Updated "Login" and "Register".. Moved "Index.html" to views along with all other view pages and are now following extensions ".ejs"

--- serverInit.js
+++ serverInit.js
@@ -1,13 +1,31 @@
-const express = require("express");
-const path = require("path");
-const app = express();
+const express = require("express")
+const path = require("path")
+const server = express()
+
+server.set("view-engine", "ejs")  //using the ejs view engine
+server.use(express.urlencoded({  extended: false  }))
+
+//Routing
+server.get('/', (req, res) => {
+    res.render('index.ejs')
+});
+
+server.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+server.get('/register', (req,res) => {
+    res.render('register.ejs')
+})
+
+
+//Why use post requests?
+server.post('/login', (req, res) =>{
+    req.body.username  //This is where I stopped: 10:42 Node.js Passport Login System tutorial
+                        //the "username" correspondes to the name field in an html tag
+})
 
 
 //Tells us what port this server is on
-app.listen(3000, () => console.log("Listening at port 3000"));
-
-//Lets get this server to grab index.html
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
+server.listen(3000, () => {
+    console.log("Listening at port 3000")});
