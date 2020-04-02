d6d8e8acf75edd98f970f3049057ef15ee029d95
Sabir Kirpal <skirpal2000@gmail.com>
2020-04-01 23:22:06 -0700

    Server now uses the stylesheet for index.ejs

--- serverInit.js
+++ serverInit.js
@@ -5,22 +5,24 @@ const bodyParser = require("body-parser")
 const path = require("path")
 const server = express()
 
-
+server.use(express.static(__dirname + '/public'))
 server.use(express.urlencoded({  extended: false  }))
 server.use(bodyParser.urlencoded({extended: true}))
 
 
 //MongoClient.connect('', (err, database) =>{
-
-
 //})
 
+server.set("view engine", "ejs")
 
 //Routing
 server.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html")
-});
+    res.render("pages/index")
+})
 
+server.get("/foodreserve", (req, res) => {
+    res.render("pages/inventory")
+})
 
 server.post('/login', (req, res) =>{
     req.body.username  
@@ -29,6 +31,8 @@ server.post('/login', (req, res) =>{
 server.post("/loginAttempt", (req, res) =>{
     console.log(req.body)
 })
+
+
 
 //Tells us what port this server is on
 server.listen(3000, () => {
